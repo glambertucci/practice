840d2c404bf64e9e57f351de4a22efe6221381bc
glambertucci <glambertucci@itba.edu.ar>
2017-04-27 19:10:09 -0300

    Added the explicit cast of the pointers

--- Codecopy.c
+++ Codecopy.c
@@ -1,11 +1,11 @@
 
-void gen_swap(void *source, void *destination, unsigned int bytes);
+void gen_swap(void *source, void *destination, unsigned int bytes)
 {
 	char * copy_source;
 	char * copy_destination;
 	int index=0;
-	copy_source=source;
-	copy_destination=destination;
+	copy_source=(char *)source;
+	copy_destination=(char *)destination;
 	while (bytes)
 	{
 		*(copy_destination+index)=*(copy_source+index);
